b7a9a3e4f542e4533367e46faad8f35d185f923c
Artem Sheremeta <sheremeta.ao@gmail.com>
2024-03-26 15:33:10 +0200

    Test coverage of the all file 'buttonHadlers.ts'

--- test/buttonHandler.test.ts
+++ test/buttonHandler.test.ts
@@ -3,6 +3,12 @@ import {
   clickStart,
   clickMenu,
   menuBank,
+  manual,
+  clickBack,
+  getListBank,
+  help,
+  statistics,
+  back,
 } from "../src/mainHandlers/buttonHandlers";
 import { User } from "../src/models/user";
 import { Limit } from "../src/models/limit";
@@ -83,6 +89,22 @@ describe("clickStart function", () => {
       })
     );
   });
+
+  it("should send a message if the user does not have a username", async () => {
+    const bot = {
+      sendMessage: jest.fn(),
+    } as any;
+
+    const msg: Message = {
+      chat: { id: 1, username: null, first_name: "Test" },
+    } as any;
+
+    await clickStart(msg, bot, {});
+    expect(bot.sendMessage).toHaveBeenCalledWith(
+      1,
+      "Ваш обліковий запис Telegram не має імені користувача."
+    );
+  });
 });
 
 describe("clickMenu function", () => {
@@ -156,5 +178,151 @@ describe("menuBank function", () => {
         reply_markup: expect.any(Object),
       })
     );
+  });
+});
+
+describe("menu function", () => {
+  it("should send a message with a Telegram link", () => {
+    const bot = {
+      sendMessage: jest.fn(),
+    } as any;
+
+    const msg: Message = {
+      chat: { id: 1 },
+    } as any;
+
+    manual(msg, bot);
+    expect(bot.sendMessage).toHaveBeenCalledWith(
+      1,
+      expect.stringContaining("https://t.me/cryptostratagems"),
+      expect.objectContaining({ parse_mode: "HTML" })
+    );
+  });
+});
+
+describe("clickBack function", () => {
+  it("should update userContext and send a message with a keyboard", () => {
+    const bot = {
+      sendMessage: jest.fn(),
+    } as any;
+
+    const msg: Message = {
+      chat: { id: 1 },
+    } as any;
+
+    const userContext = {
+      1: {
+        currentKey: "someKey",
+        isSettingLimit: true,
+        isDeposit: true,
+        isTransfer: true,
+        isSumBuy: true,
+        isCourseBuy: true,
+        isCourseSell: true,
+        isCommission: true,
+      },
+    };
+
+    clickBack(msg, bot, userContext);
+    expect(userContext[1].isSettingLimit).toBe(false);
+    expect(userContext[1].isDeposit).toBe(false);
+    expect(userContext[1].isTransfer).toBe(false);
+    expect(userContext[1].isSumBuy).toBe(false);
+    expect(userContext[1].isCourseBuy).toBe(false);
+    expect(userContext[1].isCourseSell).toBe(false);
+    expect(userContext[1].isCommission).toBe(false);
+
+    expect(bot.sendMessage).toHaveBeenCalledWith(
+      1,
+      "⬅️ Повернення до попереднього меню... ⬅️",
+      expect.objectContaining({
+        reply_markup: expect.objectContaining({ resize_keyboard: true }),
+      })
+    );
+  });
+});
+
+describe("getListBank function", () => {
+  it("should send a message with a list of banks", () => {
+    const bot = {
+      sendMessage: jest.fn(),
+    } as any;
+
+    const msg: Message = {
+      chat: { id: 1 },
+    } as any;
+
+    getListBank(msg, bot);
+
+    expect(bot.sendMessage).toHaveBeenCalledWith(
+      1,
+      "Виберіть банк:",
+      expect.objectContaining({
+        reply_markup: expect.objectContaining({
+          inline_keyboard: expect.any(Array),
+        }),
+      })
+    );
+  });
+});
+
+describe("help function", () => {
+  it("should send a help message with contact information", () => {
+    const bot = {
+      sendMessage: jest.fn(),
+    } as any;
+
+    const msg: Message = {
+      chat: { id: 1 },
+    } as any;
+
+    help(msg, bot);
+
+    expect(bot.sendMessage).toHaveBeenCalledWith(
+      1,
+      expect.stringContaining(
+        "Наші хелпери готові відповісти на ваші запитання"
+      ),
+      expect.objectContaining({ parse_mode: "HTML" })
+    );
+  });
+});
+
+describe("statistics function", () => {
+  it("should send a message with bank statistics", () => {
+    const bot = {
+      sendMessage: jest.fn(),
+    } as any;
+
+    const msg: Message = {
+      chat: { id: 1 },
+    } as any;
+
+    statistics(msg, bot);
+
+    expect(bot.sendMessage).toHaveBeenCalledWith(
+      1,
+      expect.stringContaining("PrivatBank 🌝:")
+    );
+  });
+});
+
+describe("back function", () => {
+  it("should return 'mainMenu' when the current key is 'menuButton'", () => {
+    const currentKey = "menuButton";
+    const expectedKey = "mainMenu";
+
+    const result = back(currentKey);
+
+    expect(result).toEqual(expectedKey);
+  });
+
+  it("should return 'mainMenu' when the current key is not recognized", () => {
+    const currentKey = "unknownKey";
+    const expectedKey = "mainMenu";
+
+    const result = back(currentKey);
+
+    expect(result).toEqual(expectedKey);
   });
 });
